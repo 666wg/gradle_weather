bba76b34d788f74d2519b5acedec6b9c8d1b5dd0
666wg <2468305507@qq.com>
2018-05-19 23:15:11 +0800

    集成Thymeleaf模板引擎，Bootstrap框架

---
+++ micro-weather-report/src/main/java/cn/edu/microweatherreport/MicroWeatherReportApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.microweatherreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWeatherReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroWeatherReportApplication.class, args);
+	}
+}

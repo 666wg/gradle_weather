7f22afdbcf138e0c944f75de870960c42bf344ca
666wg <2468305507@qq.com>
2018-05-19 22:40:01 +0800

    Quartz Scheduler定时器

---
+++ micro-weather-quartz/src/main/java/cn/edu/microweatherquartz/MicroWeatherQuartzApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.microweatherquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWeatherQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroWeatherQuartzApplication.class, args);
+	}
+}
---
+++ micro-weather-quartz/src/main/java/cn/edu/microweatherquartz/config/QuartzConfiguration.java
@@ -0,0 +1,47 @@
+package cn.edu.microweatherquartz.config;
+
+import cn.edu.microweatherquartz.job.WeatherDataSyncJob;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Quartz Configuration.
+ * 
+ * @since 1.0.0 2017年11月23日
+ * @author <a href="https://waylau.com">Way Lau</a> 
+ */
+@Configuration
+public class QuartzConfiguration {
+
+	private static final int TIME = 1800; // 更新频率,每过1800s , 执行WeatherDataSyncJob.class
+
+	// JobDetail
+	@Bean
+	public JobDetail weatherDataSyncJobDetail() {
+		return JobBuilder.newJob(WeatherDataSyncJob.class)
+						.withIdentity("weatherDataSyncJob")
+						.storeDurably()
+						.build();
+	}
+	
+	// Trigger
+	@Bean
+	public Trigger weatherDataSyncTrigger() {
+		
+		SimpleScheduleBuilder schedBuilder = SimpleScheduleBuilder.simpleSchedule()
+																.withIntervalInSeconds(TIME)
+																.repeatForever();
+		
+		return TriggerBuilder.newTrigger()
+							.forJob(weatherDataSyncJobDetail())
+							.withIdentity("weatherDataSyncTrigger")
+							.withSchedule(schedBuilder)
+							.build();
+	}
+}

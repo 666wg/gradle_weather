7acb74ba37d4c73a828d889fbbcefc1a42057538
666wg <2468305507@qq.com>
2018-05-20 16:32:11 +0800

    服务拆分

---
+++ msa-weather-data-server/src/main/java/cn/edu/msaweatherdataserver/MsaWeatherDataServerApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.msaweatherdataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaWeatherDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaWeatherDataServerApplication.class, args);
+	}
+}
